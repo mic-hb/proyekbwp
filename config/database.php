414aa8fa6807c429acae561ce49786e98b82b898
Melvin Satria <126858147+jinkisaragi22@users.noreply.github.com>
2023-12-05 21:59:41 +0700

    Revert "Mb"

--- config/database.php
+++ config/database.php
@@ -63,26 +63,6 @@ return [
             ]) : [],
         ],
 
-        'db_hotel_connection' => [
-            'driver' => 'mysql',
-            'url' => env('DATABASE_URL'),
-            'host' => env('DB_HOTEL_HOST', '127.0.0.1'),
-            'port' => env('DB_HOTEL_PORT', '3306'),
-            'database' => env('DB_HOTEL_DATABASE', 'forge'),
-            'username' => env('DB_HOTEL_USERNAME', 'forge'),
-            'password' => env('DB_HOTEL_PASSWORD', ''),
-            'unix_socket' => env('DB_HOTEL_SOCKET', ''),
-            'charset' => 'utf8mb4',
-            'collation' => 'utf8mb4_unicode_ci',
-            'prefix' => '',
-            'prefix_indexes' => true,
-            'strict' => true,
-            'engine' => null,
-            'options' => extension_loaded('pdo_mysql') ? array_filter([
-                PDO::MYSQL_ATTR_SSL_CA => env('MYSQL_ATTR_SSL_CA'),
-            ]) : [],
-        ],
-
         'pgsql' => [
             'driver' => 'pgsql',
             'url' => env('DATABASE_URL'),
@@ -145,7 +125,7 @@ return [
 
         'options' => [
             'cluster' => env('REDIS_CLUSTER', 'redis'),
-            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_') . '_database_'),
+            'prefix' => env('REDIS_PREFIX', Str::slug(env('APP_NAME', 'laravel'), '_').'_database_'),
         ],
 
         'default' => [
