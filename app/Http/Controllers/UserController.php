36c5dad6b45270dc34c76aae77cfce6a0f52e52e
mic-hb <michael.h22@mhs.istts.ac.id>
2023-12-06 20:25:05 +0700

    Setup user controller (login & register)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\User;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getLoginPage()
+    {
+        return view('login');
+    }
+
+    public function getRegisterPage()
+    {
+        return view('register');
+    }
+
+    public function getLogout()
+    {
+    }
+
+    public function postLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required|email',
+            'password' => 'required|alpha_num|min:8'
+        ]);
+
+        $credentials = $request->only('email', 'password');
+
+        if (auth()->attempt($credentials)) {
+            return redirect()->route('home-page');
+        }
+
+        return redirect()->back()->with('error', 'Invalid credentials');
+    }
+
+    public function postRegister(Request $request)
+    {
+        $request->validate([
+            'name' => 'required|alpha_num|min:3',
+            'email' => 'required|email|unique:users',
+            'password' => 'required|alpha_num|min:8|confirmed'
+        ]);
+
+        $user = new User();
+        $user->name = $request->input('name');
+        $user->email = $request->input('email');
+
+        $user->password = bcrypt($request->input('password'));
+
+        $user->save();
+
+        return redirect()->route('login-page')->with('success', 'You have been registered successfully');
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -18,4 +19,12 @@ Route::redirect('home', '/', 301);
 
 Route::controller(MainController::class, 'getHomePage')->group(function () {
     Route::get('/', 'getHomePage')->name('home-page');
+});
+
+Route::controller(UserController::class)->group(function () {
+    Route::get('/login', 'getLoginPage')->name('login-page');
+    Route::post('/login', 'postLogin')->name('login');
+    Route::get('/logout', 'getLogout')->name('logout');
+    Route::get('/register', 'getRegisterPage')->name('register-page');
+    Route::post('/register', 'postRegister')->name('register');
 });
