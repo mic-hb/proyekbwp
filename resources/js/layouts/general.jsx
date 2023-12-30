5b6375abba1f1d86a6a3d8425b45a271e143e719
BadutSaurus <badutsaurus@gmail.com>
2023-12-31 00:11:51 +0700

    Update layout and route names

--- resources/js/layouts/general.jsx
+++ resources/js/layouts/general.jsx
@@ -3,7 +3,7 @@ import Footer from '@/components/footer'
 
 export default function GeneralLayout({ children }) {
   return (
-    <div className='min-h-screen flex flex-col'>
+    <div className='min-h-screen flex flex-col justify-between'>
       <NavBar />
       <div className='max-w-screen-lg container mx-auto px-4 py-8'>
         {children}
--- routes/web.php
+++ routes/web.php
@@ -22,8 +22,8 @@ use Inertia\Inertia;
 
 Route::redirect('home', '/', 301);
 
-Route::inertia('/', 'home')->name('home-page');
-Route::inertia('/hotels', 'all-hotels')->name('all-hotels-page');
+Route::inertia('/', 'index')->name('home-page');
+Route::inertia('/hotels', 'hotels')->name('all-hotels-page');
 Route::inertia('/hotel/{id}', 'detail-hotel')->name('hotel-page');
 
 /**
