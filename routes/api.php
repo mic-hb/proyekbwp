b8e8910ae4fdafce27c7b8163ade0ba1f63db94a
Rafael-Matthew <rafaelmatthew2305@gmail.com>
2024-01-06 01:57:11 +0700

    Fixing API and ROUTING

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,10 @@
 <?php
 
+use App\Http\Controllers\AdminController;
+use App\Http\Controllers\BookingController;
 use App\Http\Controllers\MainController;
+use App\Http\Controllers\ManagerController;
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -55,3 +59,84 @@ Route::get('/testpassword/', function (Request $request) {
 
     return $hashedPassword;
 });
+
+/**
+ *  Route untuk halaman-halaman utama, cth: home, halaman hotel, halaman kamar, dll.
+ */
+Route::controller(MainController::class, 'getHomePage')->group(function () {
+    // Route::get('/', 'getHomePage')->name('home-page');
+    // Route::get('/hotels', 'getHotelsPage')->name('hotels-page');                                            // list hotel yang ada
+    // Route::get('/hotels/{hotel_code}', 'getHotelDetailPage')->name('hotel-detail-page');                    // detail hotel
+    // Route::get('/hotels/{hotel_code}/{room_type_code}', 'getRoomDetailPage')->name('room-detail-page');     // detail jenis kamar
+
+    // TODO: 1. kombinasi untuk booking di-hash atau di-random menjadi token (hotel_code, room_code, user_id)
+    // TODO: 2. simpan ke session atau sebagai booking_id (hanya semacam token sementara)
+    // TODO: 3. redirect ke halaman booking sesuai dengan id hotel & id kamar yang dipilih
+    // TODO: 4. tampilkan data booking berdasarkan booking_id atau token yang di-hash/di-random tadi (harus bisa di decode/un-hash)
+    // Route::post('hotels/{hotel_code}/{room_type_code}/setup', 'postSetupBooking')->name('setup-booking');    // proses data mengenai hotel & kamar yang di booking, redirect ke halaman booking
+});
+
+
+/**
+ *  Route untuk halaman-halaman yang berhubungan dengan hotel, cth: booking hotel, detail booking, dll.
+ */
+Route::controller(BookingController::class)->group(function () {
+    Route::middleware(['CekRole:1'])->group(function () {
+        Route::post('/bookings/setup', 'postSetupBooking')->name('setup-booking');                             // proses data mengenai hotel & kamar yang di booking, redirect ke halaman booking
+    });
+
+    // Route::get('hotels/book/{booking_token}', 'getBookingProcessPage')->name('book-hotel-page');            // halaman booking, user mengisi data diri, tanggal, dll.
+    // Route::post('hotels/book', 'postBookingProcess')->name('book-hotel');                                   // proses transaction booking, simpan ke database (termasuk dtrans htrans)
+
+    // // TODO: 5. tampilkan data booking berdasarkan booking_id yang sesungguhnya di database
+    // Route::get('{user_username}/{booking_id}', 'getBookingPage')->name('booking-page');                     // detail booking (hanya bisa diakses user yang login)
+});
+
+
+/**
+ *  Route untuk halaman-halaman yang berhubungan dengan user, cth: login, register, logout, dll.
+ */
+Route::controller(UserController::class)->group(function () {
+    Route::get('/profile', 'getProfilePage')->name('profile-page')->middleware(['CekRole:1']);                               // data user + list favorite & review
+
+    Route::get('/logout', 'getLogout')->name('logout');
+    Route::post('/postLogin', 'postLogin')->name('login');
+    Route::post('/postRegister', 'postRegister')->name('register');
+});
+
+
+/**
+ *  Route untuk admin
+ */
+Route::controller(AdminController::class)->group(function () {
+    Route::middleware(['CekRole:0'])->group(function () {
+        Route::prefix('admin')->group(function () {
+            Route::get('/', 'getAdminPage')->name('admin-page');
+            Route::post('/proses', 'doProses')->name('doProses');
+            Route::post('/delete', 'doDelete')->name('doDelete');
+        });
+    });
+});
+
+
+/**
+ *  Route untuk manager hotel / seller
+ */
+Route::controller(ManagerController::class)->group(function () {
+    Route::middleware(['CekRole:2'])->group(function () {
+        Route::get('/manager', 'getDashboardPage')->name('dashboard-page');
+    });
+});
+
+
+// Route::inertia('/', 'index');
+
+// Route::get('/', function () {
+//     return Inertia::render('index', [
+//         'canLogin' => Route::has('login'),
+//         'canRegister' => Route::has('register'),
+//         'laravelVersion' => Application::VERSION,
+//         'phpVersion' => PHP_VERSION,
+//         'namaKita' => "Budi",
+//     ]);
+// });
