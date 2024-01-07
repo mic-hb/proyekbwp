dae64a542e7a670d4b6536943b3caeaed4c60fbe
LeonWidjaja <leonwidjaja1405@gmail.com>
2024-01-07 23:40:26 +0700

    Tampilan Report

--- resources/js/Pages/admin.jsx
+++ resources/js/Pages/admin.jsx
@@ -97,10 +97,18 @@ export default function admin() {
                 <tbody>
                     {hotels.map((hotel) => (
                         <tr>
-                            <td className="border border-black px-2 py-1">{hotel.code}</td>
-                            <td className="border border-black px-2 py-1">{hotel.name}</td>
-                            <td className="border border-black px-2 py-1">{hotel.address}</td>
-                            <td className="border border-black px-2 py-1">{hotel.city_name}</td>
+                            <td className="border border-black px-2 py-1">
+                                {hotel.code}
+                            </td>
+                            <td className="border border-black px-2 py-1">
+                                {hotel.name}
+                            </td>
+                            <td className="border border-black px-2 py-1">
+                                {hotel.address}
+                            </td>
+                            <td className="border border-black px-2 py-1">
+                                {hotel.city_name}
+                            </td>
                             <td className="border border-black px-2 py-1">
                                 <button
                                     type="submit"
@@ -113,6 +121,23 @@ export default function admin() {
                     ))}
                 </tbody>
             </table>
+
+            <div className="flex w-full space-x-10 justify-center">
+                <h1 className="text-2xl font-bold mt-5">Report Hotel</h1>
+                <select name="report" id="listReport" className="w-1/5 mt-3">
+                    <option value="H001">H001</option>
+                    <option value="H002">H002</option>
+                    <option value="H003">H003</option>
+                    <option value="H004">H004</option>
+                    <option value="H005">H005</option>
+                    <option value="H006">H006</option>
+                    <option value="H007">H007</option>
+                    <option value="H008">H008</option>
+                    <option value="H009">H009</option>
+                    <option value="H0010">H0010</option>
+                </select>
+                <button className="px-2 py-1 bg-gradient-to-r from-purple-600 via-indigo-600 to-pink-500 rounded-lg w-1/4 text-white mt-3">Show Report</button>
+            </div>
         </GeneralLayout>
     );
 }
--- resources/js/Pages/invoice.jsx
+++ resources/js/Pages/invoice.jsx
@@ -5,74 +5,201 @@ import api from "@/api";
 export default function invoice() {
     return (
         <GeneralLayout>
-            <h1 className="text-3xl font-bold flex">INVOICE</h1>
+            <h1 className="text-3xl font-bold flex">
+                REPORT HOTEL Hotel Enak Enak
+            </h1>
             <div className="flex flex-col w-full gap-4 px-8 py-6 shadow-lg rounded-lg">
-                <table className="w-1/3">
+                <table className="w-2/3">
                     <tr>
-                        <td>Invoice No</td>
+                        <td>Hotel Code</td>
                         <td>:</td>
-                        <td>1</td>
+                        <td>H001</td>
                     </tr>
                     <tr>
-                        <td>Date</td>
+                        <td>Hotel Name</td>
                         <td>:</td>
-                        <td>32 February 2150</td>
+                        <td>Hotel Enak Enak</td>
                     </tr>
                     <tr>
-                        <td>Name</td>
+                        <td>City</td>
                         <td>:</td>
-                        <td>Budi</td>
+                        <td>Madura</td>
                     </tr>
                     <tr>
-                        <td>Email</td>
+                        <td>Address</td>
                         <td>:</td>
-                        <td>budi@gmail.com</td>
+                        <td>
+                            47062 Mortimer Groves Apt. 780 Hirthemouth, AK 66576
+                        </td>
                     </tr>
                     <tr>
-                        <td>Phone Number</td>
+                        <td>Number Of Transactions</td>
                         <td>:</td>
-                        <td>+628181818181</td>
+                        <td>2</td>
                     </tr>
                 </table>
-                <table className="w-full justify-center text-center items-center">
-                    <thead>
+            </div>
+
+            <h1 className="text-3xl font-bold flex justify-center mt-5">
+                INVOICE
+            </h1>
+            <div className="flex flex-col w-full gap-4 px-8 py-6 shadow-lg rounded-lg bg-slate-400 mt-5">
+                <div className="flex flex-col w-full gap-4 px-8 py-6 shadow-lg rounded-lg bg-white">
+                    <table className="w-1/3">
                         <tr>
-                            <th className="border border-black">Hotel Code</th>
-                            <th className="border border-black">Hotel Name</th>
-                            <th className="border border-black">Room Type</th>
-                            <th className="border border-black">Subtotal</th>
+                            <td>Invoice No</td>
+                            <td>:</td>
+                            <td>1</td>
                         </tr>
-                    </thead>
-                    <tbody>
                         <tr>
-                            <td className="border border-black">H001</td>
-                            <td className="border border-black">
-                                Hotel Enak-Enak
-                            </td>
-                            <td className="border border-black">
-                                Kamar Kedap Suara
-                            </td>
-                            <td className="border border-black">Rp. 399000</td>
+                            <td>Date</td>
+                            <td>:</td>
+                            <td>32 February 2150</td>
                         </tr>
-                    </tbody>
-                </table>
-                <table className="w-1/4">
-                    <tr>
-                        <td>Total</td>
-                        <td>:</td>
-                        <td>Rp. 399000</td>
-                    </tr>
-                    <tr>
-                        <td>Payment Method</td>
-                        <td>:</td>
-                        <td>Bank Transfer</td>
-                    </tr>
-                    <tr>
-                        <td>Payment Status</td>
-                        <td>:</td>
-                        <td>Paid</td>
-                    </tr>
-                </table>
+                        <tr>
+                            <td>Name</td>
+                            <td>:</td>
+                            <td>Budi</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td>budi@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Phone Number</td>
+                            <td>:</td>
+                            <td>+628181818181</td>
+                        </tr>
+                    </table>
+                    <table className="w-full justify-center text-center items-center">
+                        <thead>
+                            <tr>
+                                <th className="border border-black">
+                                    Hotel Code
+                                </th>
+                                <th className="border border-black">
+                                    Hotel Name
+                                </th>
+                                <th className="border border-black">
+                                    Room Type
+                                </th>
+                                <th className="border border-black">
+                                    Subtotal
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="border border-black">H001</td>
+                                <td className="border border-black">
+                                    Hotel Enak-Enak
+                                </td>
+                                <td className="border border-black">
+                                    Kamar Kedap Suara
+                                </td>
+                                <td className="border border-black">
+                                    Rp. 399000
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="w-1/4">
+                        <tr>
+                            <td>Total</td>
+                            <td>:</td>
+                            <td>Rp. 399000</td>
+                        </tr>
+                        <tr>
+                            <td>Payment Method</td>
+                            <td>:</td>
+                            <td>Bank Transfer</td>
+                        </tr>
+                        <tr>
+                            <td>Payment Status</td>
+                            <td>:</td>
+                            <td>Paid</td>
+                        </tr>
+                    </table>
+                </div>
+                <div className="flex flex-col w-full gap-4 px-8 py-6 shadow-lg rounded-lg bg-white">
+                    <table className="w-1/3">
+                        <tr>
+                            <td>Invoice No</td>
+                            <td>:</td>
+                            <td>2</td>
+                        </tr>
+                        <tr>
+                            <td>Date</td>
+                            <td>:</td>
+                            <td>32 February 2150</td>
+                        </tr>
+                        <tr>
+                            <td>Name</td>
+                            <td>:</td>
+                            <td>Budi</td>
+                        </tr>
+                        <tr>
+                            <td>Email</td>
+                            <td>:</td>
+                            <td>budi@gmail.com</td>
+                        </tr>
+                        <tr>
+                            <td>Phone Number</td>
+                            <td>:</td>
+                            <td>+628181818181</td>
+                        </tr>
+                    </table>
+                    <table className="w-full justify-center text-center items-center">
+                        <thead>
+                            <tr>
+                                <th className="border border-black">
+                                    Hotel Code
+                                </th>
+                                <th className="border border-black">
+                                    Hotel Name
+                                </th>
+                                <th className="border border-black">
+                                    Room Type
+                                </th>
+                                <th className="border border-black">
+                                    Subtotal
+                                </th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            <tr>
+                                <td className="border border-black">H001</td>
+                                <td className="border border-black">
+                                    Hotel Enak-Enak
+                                </td>
+                                <td className="border border-black">
+                                    Kamar Kedap Suara
+                                </td>
+                                <td className="border border-black">
+                                    Rp. 399000
+                                </td>
+                            </tr>
+                        </tbody>
+                    </table>
+                    <table className="w-1/4">
+                        <tr>
+                            <td>Total</td>
+                            <td>:</td>
+                            <td>Rp. 399000</td>
+                        </tr>
+                        <tr>
+                            <td>Payment Method</td>
+                            <td>:</td>
+                            <td>Bank Transfer</td>
+                        </tr>
+                        <tr>
+                            <td>Payment Status</td>
+                            <td>:</td>
+                            <td>Paid</td>
+                        </tr>
+                    </table>
+                </div>
             </div>
         </GeneralLayout>
     );
